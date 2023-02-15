43c0f40251b1626f257d51dd4886f6b3c2b10848
alyssa-cheng <alyssacheng@g.ucla.edu>
2023-02-15 14:57:40 -0800

    DRAFT 2
    Mostly finished all functions but haven't checked if they work. Website is bringing up a TypeError.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask import render_template, abort, redirect, url_for
-from flask import request
+from flask import render_template
+from flask import request, g
 import sqlite3
 
 app = Flask(__name__)
@@ -12,7 +12,69 @@ def get_message_db():
           return g.message_db
   except:
           g.message_db = sqlite3.connect("messages_db.sqlite")
-          cmd = '' # replace this with your SQL query
+
+          cmd = \
+          """
+          CREATE TABLE IF NOT EXISTS messages
+          (id INTEGER, handle TEXT, message TEXT)
+          """
+
           cursor = g.message_db.cursor()
           cursor.execute(cmd)
           return g.message_db
+  
+def insert_message(request):
+        msg = request.form['message']
+        handle = request.form['handle']
+        # id = 1 + rows of database
+
+        with get_message_db() as db:
+                cursor = db.cursor()
+
+                cursor.execute("SELECT COUNT(*) FROM messages")
+                id = cursor.fetchone()[0] + 1
+
+                cmd = \
+                f"""
+                INSERT INTO messages(id, handle, message)
+                VALUES({id}, '{handle}', '{msg}')"
+                """
+
+                cursor.execute(cmd)
+                db.commit()
+                g.message_db.close()
+
+        return [msg, handle]
+
+def render_submit():
+        if request.method == "GET":
+               return render_template("submit.html")
+        else:
+               message, handle = insert_message()
+               return render_template("submit.html",
+                                      message,
+                                      handle,
+                                      thanks = True)
+
+def random_messages(n):
+        with get_message_db() as db:
+                cursor = db.cursor()
+
+                cmd = \
+                f"""
+                SELECT message, handle
+                FROM messages
+                ORDER BY RANDOM()
+                LIMIT {n}
+                """
+
+                cursor.execute(cmd)
+                # returns a list of tuples with message and handle
+                result = [cursor.fetchone() for i in range(n)]
+                g.message_db.close()
+
+        return result
+
+def render_view():
+       MsgHandleList = random_messages(5)
+       return render_template("view.html")
